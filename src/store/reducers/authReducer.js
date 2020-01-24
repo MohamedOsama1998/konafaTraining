9ce61f304e86533521c9e5657b00aa87e5ee76f1
Mohamed Osama <mohamedosama98x@gmail.com>
2020-01-25 00:42:56 +0200

    Setting things up: Redux, Router

---
+++ src/store/reducers/authReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  signInErr: null,
+  signUpErr: null
+};
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
